d435753bfcd0f7056ec2ac2ed9bf7be12f5d0cdd
Steven Rosales <stevenrosales31@gmail.com>
2023-11-14 22:05:35 -0500

    Add calification section in assistant

--- src/components/templates/MainContent.vue
+++ src/components/templates/MainContent.vue
@@ -10,7 +10,6 @@
         <div class="fixed bottom-4 right-4">
           <div class="bg-white shadow-md p-4 rounded-lg ">
             <div>
-
               <div class="flex-1  justify-between flex flex-col h-96 w-80">
                 <div class="flex sm:items-center justify-between border-b-2 border-gray-200">
                   <div class="relative flex items-center">
@@ -58,15 +57,52 @@
                       <UserIcon />
                     </div>
                   </div>
+                  <!-- mensaje para la falificación -->
+                  <div v-if="calificando" class="flex items-end">
+                    <div class="flex flex-col  text-xs max-w-xs mx-2 order-2 items-start">
+                      <div>
+                        <div
+                          class="px-2 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
+                          <span class="font-bold">
+                            Califica la atención del asistente virtual
+
+                          </span>
+                          <div @click="calificar(1)" class="flex items-center cursor-pointer hover:text-green-900  text-green-500">
+                            <FaceHappyIcon  class="text-xl "/>
+                            <span class="font-bold  ">Buena</span>
+                          </div>
+                          <div @click="calificar(2)" class="flex items-center cursor-pointer text-orange-300 hover:text-orange-900">
+                            <FaceNeutralIcon  class=" text-xl "/>
+                            <span class="font-bold ">Regular</span>
+                          </div>
+                          <div @click="calificar(3)" class="flex items-center cursor-pointer text-red-400 hover:text-red-900">
+                            <FaceSadIcon  class="text-xl"/>
+                            <span class="font-bold ">Mala</span>
+                          </div>
+                          <div @click="cancelCalificar">
+                            <span  class="cursor-pointer text-gray-400 hover:text-gray-900"
+                            >
+                              En otro momento
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <span class="text-xs text-gray-600">{{ new Date().toLocaleDateString() }}</span>
+                    </div>
+                    <div class="w-6 h-6 rounded-full order-1">
+                      <BotIcon />
+                    </div>
+
+                  </div>
                 </div>
                 <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
                   <div class="relative flex">
-                    <input :disabled="isLoading" type="text" placeholder="Escribe un mensaje..." v-model="newMessage.text"
-                      v-on:keyup.enter="sendMessage(newMessage.text) " id="message-input"
+                    <input :disabled="isLoading || calificando" type="text" placeholder="Escribe un mensaje..."
+                      v-model="newMessage.text" v-on:keyup.enter="sendMessage(newMessage.text)" @input="waitForMessage"
+                      id="message-input"
                       class="w-full text-xs focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-2 bg-gray-200 rounded-md py-3 ">
                     <div class="absolute right-0 items-center inset-y-0  sm:flex">
-                      <button type="button" @click="sendMessage(newMessage.text)"
-                      :disabled="isLoading"
+                      <button type="button" @click="sendMessage(newMessage.text)" :disabled="isLoading||calificando"
                         class="inline-flex items-center justify-center rounded-lg px-2 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                           class="h-4 w-4 ml-2 transform rotate-90">
@@ -101,14 +137,20 @@ import BotIcon from '../icons/BotIcon.vue';
 import UserIcon from '../icons/UserIcon.vue';
 import FAB from '../FAB.vue';
 import { ref } from 'vue';
+import FaceHappyIcon from '../icons/FaceHappyIcon.vue';
+import FaceNeutralIcon from '../icons/FaceNeutralIcon.vue'
+import FaceSadIcon from '../icons/FaceSadIcon.vue'
 import apiComuna from '../../api/apiComuna';
 const isVisible = ref(false);
 const isLoading = ref(false);
+const timer = ref(1000)
 interface Message {
   text: string;
   from: string;
   date: string
 }
+
+const calificando = ref(false);
 
 const newMessage = ref<Message>({
   text: '',
@@ -127,7 +169,25 @@ const messages = ref<Message[]>([
     date: new Date().toLocaleString()
   },
 ]);
+
+const findMessagesSessionStorage = () => {
+  // console.log('findMessagesSessionStorage');
+  const messagesSessionStorage = sessionStorage.getItem('messages');
+  const calificandoSessionStorage = sessionStorage.getItem('calificando');
+  console.log(calificandoSessionStorage);
+  if (calificandoSessionStorage) {
+    calificando.value = calificandoSessionStorage === 'true'? true : false;
+  }
+  if (messagesSessionStorage) {
+    messages.value = JSON.parse(messagesSessionStorage);
+    // console.log(messages.value);
+  }
+};
+
+
 const sendMessage = async (text: string) => {
+  // almacenar los mensajes en session storage
+  sessionStorage.setItem('messages', JSON.stringify(messages.value));
   isLoading.value = true;
   if (text.trim() === '') return;
   newMessage.value.text = text;
@@ -148,6 +208,8 @@ const sendMessage = async (text: string) => {
       date: new Date().toLocaleString()
     };
     messages.value.push(messageBot);
+    // almacenar los mensajes en session storage
+    sessionStorage.setItem('messages', JSON.stringify(messages.value));
     // scroll to bottom of messages container
 
     newMessage.value = {
@@ -158,6 +220,7 @@ const sendMessage = async (text: string) => {
     if (messagesContainer) {
       messagesContainer.scrollTop = messagesContainer.scrollHeight;
     }
+    waitForMessage()
     isLoading.value = false;
 
   }).catch((error) => {
@@ -179,17 +242,101 @@ const sendMessage = async (text: string) => {
 
 const openChat = () => {
   isVisible.value = true;
-  console.log(isVisible.value);
-  const messagesContainer = document.getElementById('messages-container');
+  // console.log(isVisible.value);
+  const messagesContainerChat = document.getElementById('messages-container');
+  console.log(messagesContainerChat);
   // scroll to bottom of messages container
-  if (messagesContainer) {
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  if (messagesContainerChat) {
+    console.log('scroll');
+    messagesContainerChat.scrollTop = messagesContainerChat.scrollHeight;
   }
-  const messageInput = document.getElementById('message-input');
-  if (messageInput) {
-    messageInput.focus();
+  if (messages.value.length > 1 && !calificando.value) {
+    console.log('esperar');
+    waitForMessage()
   }
+  
 };
+
+const waitForMessage = () => {
+  clearTimeout(timer.value)
+  if (messages.value.length === 1) return
+  timer.value = setTimeout(() => {
+    calificando.value = true;
+    sessionStorage.setItem('calificando', JSON.stringify(calificando.value));
+    const messagesContainer = document.getElementById('messages-container');
+    sessionStorage.setItem('messages', JSON.stringify(messages.value));
+    if (messagesContainer) {
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  }, 10000)
+};
+
+const cancelCalificar =()=>{
+  calificando.value = false;
+  sessionStorage.setItem('calificando', JSON.stringify(calificando.value));
+}
+
+const calificar= async(cal:number)=>{
+  console.log(cal);
+  switch (cal) {
+    case 1:
+      await apiComuna.post("chatbot/calificar", {
+        calificacion: 1
+      }).then((response) => {
+        console.log(response);
+        calificando.value = false;
+        sessionStorage.setItem('calificando', JSON.stringify(calificando.value));
+      }).catch((error) => {
+        console.log(error);
+      }).finally(() => {
+        const messagesContainer = document.getElementById('messages-container');
+        if (messagesContainer) {
+          messagesContainer.scrollTop = messagesContainer.scrollHeight;
+        }
+      })
+      break;
+    case 2:
+      await apiComuna.post("chatbot/calificar", {
+        calificacion: 2
+      }).then((response) => {
+        console.log(response);
+        calificando.value = false;
+        sessionStorage.setItem('calificando', JSON.stringify(calificando.value));
+      }).catch((error) => {
+        console.log(error);
+      }).finally(() => {
+        const messagesContainer = document.getElementById('messages-container');
+        if (messagesContainer) {
+          messagesContainer.scrollTop = messagesContainer.scrollHeight;
+        }
+      })
+      break;
+    case 3:
+      await apiComuna.post("chatbot/calificar", {
+        calificacion: 3
+      }).then((response) => {
+        console.log(response);
+        calificando.value = false;
+        sessionStorage.setItem('calificando', JSON.stringify(calificando.value));
+      }).catch((error) => {
+        console.log(error);
+      }).finally(() => {
+        const messagesContainer = document.getElementById('messages-container');
+        if (messagesContainer) {
+          messagesContainer.scrollTop = messagesContainer.scrollHeight;
+        }
+      })
+      break;
+    default:
+      break;
+  }
+
+
+}
+
+
+findMessagesSessionStorage();
+
 
 
 </script>
